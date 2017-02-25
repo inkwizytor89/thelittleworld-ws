a22f4637b5a89a7590361e3f739814eaa09f8190
Kamil Kostyszyn <inkwizytor89@gmail.com>
2017-02-25 10:59:26 +0100

    Spring boot initial commit

---
+++ src/main/java/com/thelittleworld/ThelittleworldWsApplication.java
@@ -0,0 +1,12 @@
+package com.thelittleworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThelittleworldWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThelittleworldWsApplication.class, args);
+	}
+}
